98f38770fc2b6adf4bc7d7b90512db22224b8b6c
DWhittley <wowwit@bell.net>
2023-08-09 21:55:22 -0400

    Initial commit of less files
    took out textures and extra maps

---
+++ Source/HeliG/HeliGGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HeliGGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELIG_API AHeliGGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
