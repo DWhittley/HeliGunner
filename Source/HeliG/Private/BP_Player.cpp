50be1b554f3878070eb9df561ac3d8255ab345ae
DWhittley <wowwit@bell.net>
2023-09-17 18:33:20 -0400

    Look function interim
    Added C++ Character class and all works fine
    disconnected the BP version of look and added C++ character code to bind it and no go

---
+++ Source/HeliG/Private/BP_Player.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BP_Player.h"
+
+// Sets default values
+ABP_Player::ABP_Player()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABP_Player::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(GEngine != nullptr)
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Spawning FPSCharacter"));
+
+	UE_LOG(LogTemp, Warning, TEXT("Spawning FPSCharacter"));
+	
+}
+
+// Called every frame
+void ABP_Player::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABP_Player::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Look
+	PlayerInputComponent->BindAxis("Turn", this, &ABP_Player::AddControllerYawInput);
+	PlayerInputComponent->BindAxis("LookUp", this, &ABP_Player::AddControllerPitchInput);
+
+}
+
