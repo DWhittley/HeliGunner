3950e8f8472814f122efbbb6f8bcc64ec8ecbba3
DWhittley <wowwit@bell.net>
2023-09-19 13:54:15 -0400

    New AI step by step
    Added C++ class BTTask_FindRandomLocation

---
+++ Source/HeliG/Public/EnemyBTTask_FindRandomLocation.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/Tasks/BTTask_BlackboardBase.h"
+#include "EnemyBTTask_FindRandomLocation.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HELIG_API UEnemyBTTask_FindRandomLocation : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
