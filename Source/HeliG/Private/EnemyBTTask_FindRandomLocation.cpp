750d295929869460d4bd813aa3aacd2c65e4359c
DWhittley <wowwit@bell.net>
2023-09-19 14:09:53 -0400

    NewAI step by step
    Added BTTask_FindRandomLocation C++ files

--- Source/HeliG/Private/EnemyBTTask_FindRandomLocation.cpp
+++ Source/HeliG/Private/EnemyBTTask_FindRandomLocation.cpp
@@ -2,4 +2,43 @@
 
 
 #include "EnemyBTTask_FindRandomLocation.h"
+#include "AIController.h"
+#include "NavigationSystem.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+UEnemyBTTask_FindRandomLocation::UEnemyBTTask_FindRandomLocation()
+{
+	NodeName = TEXT("Find Random Location");
+
+	// accept only vectors
+	BlackboardKey.AddVectorFilter(this, GET_MEMBER_NAME_CHECKED(UEnemyBTTask_FindRandomLocation, BlackboardKey));
+}
+
+EBTNodeResult::Type UEnemyBTTask_FindRandomLocation::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	FNavLocation Location{};
+
+	// Get AI Pawn
+	AAIController* AIController = OwnerComp.GetAIOwner();
+	const APawn* AIPawn = AIController->GetPawn();
+
+	// Get Pawn origin
+	const FVector Origin = AIPawn->GetActorLocation();
+
+	// Obtain Navigation System and find random location
+	const UNavigationSystemV1* NavSystem = UNavigationSystemV1::GetCurrent(GetWorld());
+	if (IsValid(NavSystem) && NavSystem->GetRandomPointInNavigableRadius(Origin, SearchRadius, Location))
+	{
+		AIController->GetBlackboardComponent()->SetValueAsVector(BlackboardKey.SelectedKeyName,
+			Location.Location);
+	}
+
+	// Signal the BehaviorTreeComponent that the task finished with success
+	FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+	return EBTNodeResult::Succeeded;
+}
+
+FString UEnemyBTTask_FindRandomLocation::GetStaticDescription() const
+{
+	return FString::Printf(TEXT("Vector: %s"), *BlackboardKey.SelectedKeyName.ToString());
+}
--- Source/HeliG/Public/EnemyBTTask_FindRandomLocation.h
+++ Source/HeliG/Public/EnemyBTTask_FindRandomLocation.h
@@ -14,4 +14,13 @@ class HELIG_API UEnemyBTTask_FindRandomLocation : public UBTTask_BlackboardBase
 {
 	GENERATED_BODY()
 	
+public:
+	UEnemyBTTask_FindRandomLocation();
+
+private:
+	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+	virtual FString GetStaticDescription() const override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Search", meta = (AllowPrivateAccess = true))
+	float SearchRadius{ 500.f };
 };
